b4019f95c44461e7f52a379c174135e18dc79f04
nairv5 <104786490+nairv5@users.noreply.github.com>
2022-09-19 17:30:21 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+$usuario = $_POST ["user"] ;
+$contrasenia=$_POST ["pass"];
+
+$ckuser="admin";
+$ckpass="1234";
+
+if ($usuario==$ckuser && $contrasenia==$ckpass){
+    header ("location:https://www.google.com/");
+}
+else {
+    header ("location:error.html");
+}
+?>
